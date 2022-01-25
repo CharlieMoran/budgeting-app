6030489a22f4fd646c290e19b3d222d5d4ba4d16
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-25 10:25:46 -0500

    recommit transactions

---
+++ models/transactions.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+      date: "2021-04-01",
+      source: "Income",
+      amount: 1000,
+    },
+    {
+      date: "2021-04-01",
+      source: "Taxes",
+      amount: -300,
+    },
+    {
+      date: "2021-04-01",
+      source: "Retirement",
+      amount: -200,
+    },
+    {
+      date: "2021-04-01",
+      source: "Savings",
+      amount: -100,
+    },
+    {
+      date: "2021-04-01",
+      source: "Credit Card Payment",
+      amount: -100,
+    },
+    {
+      date: "2021-04-05",
+      source: "Monthly Birthday Money from Aunt Tilda",
+      amount: 20,
+    },
+    {
+      date: "2021-04-05",
+      source: "Coffee",
+      amount: -4,
+    },
+    {
+      date: "2021-04-05",
+      source: "Internet",
+      amount: -100,
+    },
+    {
+      date: "2021-04-03",
+      source: "Groceries",
+      amount: -76,
+    },
+    {
+      date: "2021-04-03",
+      source: "Pet Food",
+      amount: -7,
+    },
+  ];
