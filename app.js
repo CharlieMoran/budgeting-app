06054ecbbe112e230635886c3608daffb20745c4
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-25 10:46:13 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const budget = require("./controllers/budget");
+const budgeting = require("./controllers/budget");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/transactions", budget);
+app.use("/transactions", budgeting);
 
-app.get("/", (req, res)=>{
-    res.send(`Welcome to the Budgeting Server`);
+app.get("/", (require, response)=>{
+    response.send(`Welcome to the Budgeting Server`);
 });
 
-app.get("*", (req,res)=>{
+app.get("*", (require,response)=>{
     res.status(404).json({error: "Page not found"})
 });
 
