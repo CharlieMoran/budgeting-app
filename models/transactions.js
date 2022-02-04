eca7f650306969bce7f0906c3b758bcafc40f578
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-04 12:13:26 -0500

    added zeros

--- models/transactions.js
+++ models/transactions.js
@@ -2,51 +2,51 @@ module.exports = [
     {
       date: "2021-04-01",
       source: "Income",
-      amount: 1000,
+      amount: 1000.00,
     },
     {
       date: "2021-04-01",
       source: "Taxes",
-      amount: -300,
+      amount: -300.00,
     },
     {
       date: "2021-04-01",
       source: "Retirement",
-      amount: -200,
+      amount: -200.00,
     },
     {
       date: "2021-04-01",
       source: "Savings",
-      amount: -100,
+      amount: -100.00,
     },
     {
       date: "2021-04-01",
       source: "Credit Card Payment",
-      amount: -100,
+      amount: -100.00,
     },
     {
       date: "2021-04-05",
       source: "Monthly Birthday Money from Aunt Tilda",
-      amount: 20,
+      amount: 20.00,
     },
     {
       date: "2021-04-05",
       source: "Coffee",
-      amount: -4,
+      amount: -4.00,
     },
     {
       date: "2021-04-05",
       source: "Internet",
-      amount: -100,
+      amount: -100.00,
     },
     {
       date: "2021-04-03",
       source: "Groceries",
-      amount: -76,
+      amount: -76.00,
     },
     {
       date: "2021-04-03",
       source: "Pet Food",
-      amount: -7,
+      amount: -7.00,
     },
   ];
